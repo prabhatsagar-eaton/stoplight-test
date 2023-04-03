04f1838dcb1af3c14207482890d5db56c8171644
PrabhatSagar@eaton.com <PrabhatSagar@eaton.com>
2023-04-03 17:20:09 +0530

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MzcxNQ/spectral.js?branch_slug=main&token=cc1ea0f4-3d29-425f-b71c-970a65719f88";
+export default { extends: ruleset };
